c18f053eed28c04c22ea5f631d7142c4b8770246
Todd Riggleman <toddriggleman@MacBook-Pro.home>
2021-01-16 04:52:03 -0500

    added functions to allow user to click on a tile and change background color

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 console.log( "its runniing");
 for (let i = 0; i < document.querySelectorAll(".sqr").length; i++) {
     document.querySelectorAll(".sqr")[i].addEventListener("click", function (){
+        console.log(i)
         
         var colorSquare = this.innerHTML;
-        
+        console.log(colorSquare);
 
-    })
-    
+        changeBackground(colorSquare);
+        console.log(changeBackground(colorSquare));
+    }); 
 }
 
+function changeBackground(key) {
+
+    switch (key) {
+        case "Red":
+            document.body.style.backgroundColor = "red";
+        break;
+
+        case "Blue":
+            document.body.style.backgroundColor = "deepskyblue";
+        break;
+
+        case "Green":
+            document.body.style.backgroundColor = "green";
+        break;
+
+        case "Orange":
+            document.body.style.backgroundColor = "orange";
+        break;
+
+        case "Purple":
+            document.body.style.backgroundColor = "blueViolet";
+        break;
+    
+        default: console.log(colorSquare);
+
+            break;
+    }
+}
 
 
